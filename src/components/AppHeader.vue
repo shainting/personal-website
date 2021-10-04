01bfaa5b813aa147df8ef59b447082d32a3f42d4
Shain Tsai <klhmrtuse197@gmail.com>
2021-10-04 21:43:33 +0800

    專案開始

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  position: fixed;
+}
+</style>
